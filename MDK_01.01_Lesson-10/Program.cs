240ccac977ef73b09254d4397c7421db454ed40e
ImmaUnderWaterMan <semenovr310@gmail.com>
2025-01-27 21:44:15 +0300

    Добавьте файлы проекта.

---
+++ MDK_01.01_Lesson-10/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MDK_01._01_Lesson_10
+{
+    
+    internal class Program
+    {
+        public interface ISelectable 
+        {
+            void OnSelect();
+        }
+        public interface IUpdatable
+        {
+            void Refresh();
+        }
+
+        class Screen : ISelectable, IUpdatable
+        {
+            public void OnSelect()
+            {
+                Console.WriteLine("Интерфейс выбран");
+            }
+            public void Refresh()
+            {
+                Console.WriteLine("Данные обнавленны");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Screen screen = new Screen();
+            screen.OnSelect();
+            screen.Refresh();
+            Console.ReadKey();
+        }
+    }
+}
